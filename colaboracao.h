7c1a1b8112dee1be9f504a9d88fe28fa9a1c9523
mrisso <matheush.risso@gmail.com>
2016-05-19 16:23:00 -0300

    Documentação de colaboracao.h

--- colaboracao.h
+++ colaboracao.h
@@ -16,11 +16,11 @@
 #include "editor.h"
 
 /*Estrutura Colaboração (tipo opaco)
- * - nome (string)
- * - content (string) ~ Conteúdo da colaboração
- * - delete (int) ~ Flag determina se colaboração existe ou não
- * - autor (Editor *) ~ Ponteiro para seu autor na lista de editores (tipo opaco definido em editor.h)
- * - prox (Colab *) ~ Ponteiro para a propria estrutura
+ - nome (string)
+ - content (string) ~ Conteúdo da colaboração
+ - delete (int) ~ Flag determina se colaboração existe ou não
+ - autor (Editor *) ~ Ponteiro para seu autor na lista de editores (tipo opaco definido em editor.h)
+ - prox (Colab *) ~ Ponteiro para a propria estrutura
  */
 typedef struct colab Colab;
 
@@ -41,24 +41,75 @@ Colab *initListaColab(void);
 Colab *initColab(char *nome, char *content, char *autor, Editor *editores);
 
 /*Insere uma colaboração na última posição da lista de colaborações
- * inputs: ponteiro para a lista (passagem por referência) e ponteiro para nova colaboração
+ * inputs: ponteiro para a lista e ponteiro para nova colaboração
+ * outputs: nenhum
+ * pre-condicao: lista inicializada e nova estrutura inicializada
+ * pos-condicao: lista possui nova estrutura na ultima posição
  */
 void insereColab(Colab **lista, Colab *novaColab);
 
+/*Retira uma colaboração da lista de colaborações
+ * inputs: ponteiro para a lista e noma da colaboração a ser retirada
+ * outputs: retorna 0(OK) caso a contribuição tenha sido retirada e 1(NAO_ENCONTRADO) caso não exista a colaboração na lista
+ * pre-condicao: lista com contribuições não vazia e nome da contribuição valido
+ * pos-condicao: lista não possui mais a contribuição com nome xColab (string)
+ */
 int retiraColab(Colab **lista, char* xColab);
 
+/*Libera todo o espaço de memória (recursivamente) alocado pela lista de colaborações
+ * inputs: lista de colaborações
+ * outputs: ponteiro para NULL (lista vazia)
+ * pre-condicao: lista de contribuições não vazia
+ * pos-condicao: memória alocada pela lista liberada
+ */
 Colab *freeListaColab(Colab *lista);
 
+/*Retorna o campo nome da estrutura Colab
+ * input: ponteiro para colaboração desejada
+ * output: campo nome (string) da colaboração desejada
+ * pre-condicao: estrutura Colab válida e inicializada
+ * pos-condicao: nenhuma
+ */
 char *colabName(Colab *colab);
 
+/*Retorna o campo content da estrutura Colab
+ * input: ponteiro para colaboração desejada
+ * output: campo content (string) da colaboração desejada
+ * pre-condicao: estrutura Colab válida e inicializada
+ * pos-condicao: nenhuma
+ */
 char *colabContent(Colab *colab);
 
+/*Retorna o campo prox da estrutura Colab
+ * input: ponteiro para colaboração desejada
+ * output: campo prox (Colab *) da colaboração desejada
+ * pre-condicao: estrutura Colab válida e inicializada
+ * pos-condicao: nenhuma
+ */
 Colab *nextColab(Colab *colab);
 
+/*Retorna o campo autor da estrutura Colab
+ * input: ponteiro para colaboração desejada
+ * output: campo autor (Editor *) da colaboração desejada
+ * pre-condicao: estrutura Colab válida e inicializada
+ * pos-condicao: nenhuma
+ */
 Editor *colabEditor(Colab *colab);
 
+/*Atualiza o status de uma colaboração
+ * input: ponteiro para a colaboração desejada e codigo do status desejado (int)
+ * output: nenhum
+ * pre-condicao: estrutura Colab válida e inicializada
+ * pos-condicao: estrutura Colab com o campo delete alterado para int status
+ */
 void colabSetStatus(Colab *colab, int status);
 
+/*Retorna o status de uma colaboração
+ * input: ponteiro para a colaboração desejada
+ * output: campo delete (int) da colaboração desejada
+ * pre-condicao: estrutura Colab válida e inicializada
+ * pos-condicao: nenhuma
+ */
 int colabStatus(Colab *colab);
 
 #endif
